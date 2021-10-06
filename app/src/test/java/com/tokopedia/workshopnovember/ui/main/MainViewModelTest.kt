5b8d0536db04dc3b8ae4e76a6f3d4edae9b4f141
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-10-06 11:27:30 +0700

    Add test utlil

---
+++ app/src/test/java/com/tokopedia/workshopnovember/ui/main/MainViewModelTest.kt
@@ -0,0 +1,47 @@
+package com.tokopedia.workshopnovember.ui.main
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.tokopedia.workshopnovember.MainCoroutineRule
+import com.tokopedia.workshopnovember.getOrAwaitValue
+import com.tokopedia.workshopnovember.pojo.search.Doc
+import com.tokopedia.workshopnovember.repo.BookRepository
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class MainViewModelTest {
+
+    // Set the main coroutines dispatcher for unit testing.
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    var mainCoroutineRule = MainCoroutineRule()
+
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    lateinit var repo: BookRepository
+    lateinit var sut: MainViewModel
+
+    @Before
+    fun setUp() {
+        repo = mockk()
+        sut = MainViewModel(repo)
+    }
+
+    @Test
+    fun search() {
+        val list = listOf(Doc(title = "Harry Potter"))
+        coEvery { repo.searchWithQuery(any()) } returns list
+
+        sut.search("harry")
+
+        assertThat(sut.result.getOrAwaitValue().first().title, `is`(list.first().title))
+    }
+
+
+}
