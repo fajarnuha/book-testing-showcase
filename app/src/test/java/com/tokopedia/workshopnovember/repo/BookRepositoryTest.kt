a5c7ccd1c5fde43b00fca85d5b3f34d73fef9c43
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-10-06 08:38:33 +0700

    Book Repository Test

--- app/src/test/java/com/tokopedia/workshopnovember/repo/BookRepositoryTest.kt
+++ app/src/test/java/com/tokopedia/workshopnovember/repo/BookRepositoryTest.kt
@@ -1,9 +1,31 @@
 package com.tokopedia.workshopnovember.repo
 
+import com.tokopedia.workshopnovember.pojo.search.SearchResponse
+import com.tokopedia.workshopnovember.repo.cloud.BookApi
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Test
 
+@ExperimentalCoroutinesApi
 class BookRepositoryTest {
 
+    private val api = mockk<BookApi>()
+    private val sut = BookRepository(api)
 
+    @Test
+    fun `given success response then return list of docs`() = runBlockingTest {
+        val q = "foo"
+        val fakeResponse = SearchResponse(q = q)
+        coEvery { api.search(q) } returns fakeResponse
+
+        val actual = sut.searchWithQuery(q)
+
+        assertEquals(fakeResponse.docs, actual)
+    }
 
 }
--- app/src/test/java/com/tokopedia/workshopnovember/repo/FakeBookApi.kt
+++ app/src/test/java/com/tokopedia/workshopnovember/repo/FakeBookApi.kt
@@ -1,19 +1,20 @@
 package com.tokopedia.workshopnovember.repo
 
 import com.tokopedia.workshopnovember.pojo.search.Doc
+import com.tokopedia.workshopnovember.pojo.search.SearchResponse
 import com.tokopedia.workshopnovember.repo.cloud.BookApi
 
-//class FakeBookApi: BookApi {
-//    override suspend fun search(query: String): SearchResult {
-//        return SearchResult(
-//            docs = listOf(Doc(
-//                title = "Harry Potter",
-//                author_name = listOf("JK Rowling"),
-//                ...
-//            )),
-//            q = query,
-//            num_found = 20,
-//            ...
-//        )
-//    }
-//}
+class FakeBookApi : BookApi {
+    override suspend fun search(query: String): SearchResponse {
+        return SearchResponse(
+            docs = listOf(
+                Doc(
+                    title = "Harry Potter",
+                    authorName = listOf("JK Rowling"),
+                )
+            ),
+            q = query,
+            numFound = 20,
+        )
+    }
+}
