2938904901c30a39fa42a197a1bdc2c443d60807
Fajar Ulin Nuha <fajar.nuha@tokopedia.com>
2021-11-08 15:28:20 +0700

    Add fragment hilt helper

---
+++ app/src/debug/java/com/tokopedia/workshopnovember/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package com.tokopedia.workshopnovember
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()
