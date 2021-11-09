0931ae6eaae1c23da210a9c4712669842841cc75
astidhiyaa <asti.anzaria@tokopedia.com>
2021-11-09 12:03:20 +0800

    test: add basic test

---
+++ app/src/test/java/com/tokopedia/workshopnovember/ui/main/BasicTest.kt
@@ -0,0 +1,22 @@
+package com.tokopedia.workshopnovember.ui.main;
+
+
+import org.junit.Test
+
+class BasicTest {
+    @Test
+    fun `is even number`() {
+        TODO("Not yet implemented")
+    }
+
+    @Test
+    fun `is odd number`() {
+        TODO("Not yet implemented")
+    }
+}
+
+class MagicNumbers{
+    fun isEven(number: Int): Boolean = number % 2 == 0
+}
+
+
