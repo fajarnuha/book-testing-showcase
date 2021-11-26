82c2075ad735cace4ecb2f9361be79921a5d39a4
astidhiyaa <asti.anzaria@tokopedia.com>
2021-11-26 19:53:51 +0800

    fix: ut

--- app/src/test/java/com/tokopedia/workshopnovember/ui/main/BasicTest.kt
+++ app/src/test/java/com/tokopedia/workshopnovember/ui/main/BasicTest.kt
@@ -1,21 +1,31 @@
 package com.tokopedia.workshopnovember.ui.main;
 
-
+import junit.framework.Assert.assertEquals
 import org.junit.Test
 
 class BasicTest {
     @Test
     fun `given is 2 is even number`() {
-        TODO("Not yet implemented")
+        val number = 2
+        val magicNumbers = MagicNumbers()
+
+        val actual = magicNumbers.isEven(number)
+
+        assertEquals(true, actual)
     }
 
     @Test
     fun `given is 7 is even number`() {
-        TODO("Not yet implemented")
+        val number = 7
+        val magicNumbers = MagicNumbers()
+
+        val actual = magicNumbers.isEven(number)
+
+        assertEquals(false, actual)
     }
 }
 
-class MagicNumbers{
+class MagicNumbers {
     fun isEven(number: Int): Boolean = number % 2 == 0
 }
 
